bb10fb99a0d9fa5724e9f79b5fd1fa173a8d92fc
Shaodong-iMac <zhanshaoton@gmail.com>
2025-02-10 16:56:42 +0100

    add demo: 4_auto-text-effect

---
+++ 4.auto-text/script.js
@@ -0,0 +1,20 @@
+const textEl = document.getElementById("text");
+const speedEl = document.getElementById("speed");
+const text = "Now is better than never.";
+let speed = 300 / speedEl.value;
+let idx = 1;
+
+writeText();
+
+function writeText() {
+  textEl.innerText = text.slice(0, idx);
+  idx++;
+
+  if (idx > text.length) {
+    idx = 1;
+  }
+
+  setTimeout(writeText, speed);
+}
+
+speedEl.addEventListener("input", (e) => (speed = 300 / e.target.value));
